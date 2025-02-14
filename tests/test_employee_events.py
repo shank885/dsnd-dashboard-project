0ad1786bb2b83c70729ede823962a51701c723f5
Shashank Shekhar <shashankshekhar885@gmail.com>
2025-02-14 19:14:44 +0530

    buug fixes in test_employee_events.py

--- tests/test_employee_events.py
+++ tests/test_employee_events.py
@@ -52,7 +52,7 @@ def test_employee_table_exists(table_names):
 # `test_team_table_exists`
 # This function should receive the `table_names`
 # fixture as an argument
-def test_team_table_exists(table_names)
+def test_team_table_exists(table_names):
     # Assert that the string 'team'
     # is in the table_names list
     assert "team" in tables_names, "'team' table does not exist in the database"
